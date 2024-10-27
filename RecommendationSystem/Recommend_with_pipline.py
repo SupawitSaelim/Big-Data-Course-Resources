0bb21423538ed40acb1e1b7db8dc20eefa9e1dfe
Supawit Saelim <oatsupawit_159@outlook.com>
2024-10-27 14:04:21 +0700

    add_more_function_of_reccommendation

--- RecommendationSystem/Recommend_with_pipline.py
+++ RecommendationSystem/Recommend_with_pipline.py
@@ -1,12 +1,14 @@
 from pyspark.sql import SparkSession
+from pyspark.ml import Pipeline
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.recommendation import ALS
 
 spark = SparkSession.builder.appName('BookRecommendationSystem').getOrCreate()
 
 df = spark.read.csv("./RecommendationSystem/book_ratings.csv", header=True, inferSchema=True)
-
 df.printSchema()
+
+train, test = df.randomSplit([0.8, 0.2])
 
 als = ALS(
     maxIter=10,
@@ -17,16 +19,17 @@ als = ALS(
     coldStartStrategy="drop"
 )
 
-model = als.fit(df)
+pipeline = Pipeline(stages=[als])
+
+model = pipeline.fit(train)
+
+predictions = model.transform(test)
 
 evaluator = RegressionEvaluator(
     metricName="rmse",
     labelCol="rating",
     predictionCol="prediction"
 )
-
-train, test = df.randomSplit([0.8, 0.2])
-predictions = model.transform(test)
 
 rmse = evaluator.evaluate(predictions)
 print(f"Root Mean Square Error (RMSE): {rmse}")
@@ -35,20 +38,10 @@ user53_df = df.filter(df['user_id'] == 53)
 user53_predictions = model.transform(user53_df)
 user53_predictions.orderBy("prediction", ascending=False).show(truncate=False)
 
-user_recommendations = model.recommendForAllUsers(5)
+user_recommendations = model.stages[0].recommendForAllUsers(5)  
 user_recommendations.show(truncate=False)
 
-book_recommendations = model.recommendForAllItems(5)
+book_recommendations = model.stages[0].recommendForAllItems(5) 
 book_recommendations.show(truncate=False)
 
-### add on other function ####################################################
-user_subset_df = df.select("user_id").distinct() 
-user_subset_df = user_subset_df.filter(user_subset_df.user_id.isin([1, 2]))  
-user_recommendations = model.recommendForUserSubset(user_subset_df, 5)
-user_recommendations.show(truncate=False)
-
-
-item_subset_df = df.select("book_id").distinct()  
-item_subset_df = item_subset_df.filter(item_subset_df.book_id.isin([101, 102]))  
-item_recommendations = model.recommendForItemSubset(item_subset_df, 5)
-item_recommendations.show(truncate=False)
+spark.stop()
