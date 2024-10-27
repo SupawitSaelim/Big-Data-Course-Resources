b9e7acc16e5db192d311a4a42c309c8091861291
Supawit Saelim <oatsupawit_159@outlook.com>
2024-10-27 21:30:05 +0700

    add_association_and_requirement_of_environment

--- AssociationRule&Graph/association.py
+++ AssociationRule&Graph/association.py
@@ -4,26 +4,19 @@ from pyspark.ml.fpm import FPGrowth
 from pyspark.sql.functions import collect_list, array_distinct, explode, split, col
 
 spark = SparkSession.builder.appName("FPGrowthExample").getOrCreate()
+    # .config("spark.driver.port", "4040") \ .getOrCreate()
 
 data = spark.read.csv("./AssociationRule&Graph/groceries_data.csv", header=True, inferSchema=True)
-
 grouped_data = data.groupBy("Member_number").agg(collect_list("itemDescription").alias("Items"))
-
 grouped_data.show(truncate=False)
-
 grouped_data = grouped_data.withColumn("basket", array_distinct(grouped_data["Items"]))
-
 grouped_data.show(truncate=False)
-
 exploded_data = grouped_data.select("Member_number", explode("Items").alias("item"))
 
 
 separated_data = exploded_data.withColumn("item", explode(split("item", "/")))
-
 final_data = separated_data.groupBy("Member_number").agg(collect_list("item").alias("Items"))
-
 final_data = final_data.withColumn("Items", array_distinct(col("Items")))
-
 final_data.show(truncate=False)
 
 minSupport = 0.1
@@ -48,9 +41,7 @@ new_data = spark.createDataFrame(
 )
 
 new_data.show(truncate=False)
-
 predictions = model.transform(new_data)
-
 predictions.show(truncate=False)
 
 spark.stop()
